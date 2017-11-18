bbb8253843767c7289afb76b17d7fc52e569c5e1
Guillaume AMAT <guillaume@amat.io>
2017-11-18 15:45:38 +0100

    chore: Indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,48 @@
-
 const webpack = require('webpack');
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const extractCSS = new ExtractTextPlugin('OverPassLayer.css');
 
-const plugins = [
-    extractCSS
-];
+const plugins = [extractCSS];
 
 plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-        minimize: true,
-        mangle: true,
-        output: {
-          comments: false
-        },
-        compress: {
-          warnings: false
-        }
-    }),
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: true,
+    mangle: true,
+    output: {
+      comments: false
+    },
+    compress: {
+      warnings: false
+    }
+  })
 );
 
-
 module.exports = {
-    devtool: 'source-map',
-    plugins: plugins,
-    context: path.join(__dirname, 'src'),
-    entry: {
-        OverPassLayer: './OverPassLayer'
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].bundle.js'
-    },
-    externals: {
-        'leaflet': 'L',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                loader: extractCSS.extract(['css-loader', 'postcss-loader'])
-            },
-        ]
-    },
+  devtool: 'source-map',
+  plugins: plugins,
+  context: path.join(__dirname, 'src'),
+  entry: {
+    OverPassLayer: './OverPassLayer'
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  externals: {
+    leaflet: 'L'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: extractCSS.extract(['css-loader', 'postcss-loader'])
+      }
+    ]
+  }
 };
